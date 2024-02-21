3263952cb6b7fcd59471470c04b7a2e0f3f23fc2
Phoenix Stephan <chaka3900@gmail.com>
2024-02-21 11:48:00 -0700

    Finished Learning04 'Homework'

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Phoenix Stephan", "C# Program");
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine("");
+
+        MathAssignment assignment2 = new MathAssignment("Jake Simpson", "Algebra", "7.3", "8-19");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+        Console.WriteLine("");
+
+        WritingAssignment assignment3 = new WritingAssignment("Ricky Bobby", "Writing", "Talladega Nights");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());    
     }
 }
